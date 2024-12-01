190f7d0a9388cad1c1267440aa9a00d3cbd865e1
kieran-l-jimenez <84421936+kieran-l-jimenez@users.noreply.github.com>
2024-11-30 20:05:53 -0500

    Complete Version 3

--- sim.java
+++ sim.java
@@ -17,9 +17,6 @@ class sim {
                     int B = Integer.parseInt(args[1]);
                     tracefileString = args[2];
 
-                    // validate inputs
-                    //if (B <= 0 || tracefileString == null);
-
                     // open file
                     fileReader = new BufferedReader(new FileReader(tracefileString));
 
@@ -43,13 +40,14 @@ class sim {
                             mispredictions++;
                     }
 
+                    // print results
                     myWriter.write("COMMAND\n");
                     myWriter.write("./sim smith " + B + " " + tracefileString + "\n");
                     myWriter.write("OUTPUT\n");
-                    myWriter.write("number of predictions:\t" + accesses + "\n");
+                    myWriter.write("number of predictions:\t\t" + accesses + "\n");
                     myWriter.write("number of mispredictions:\t" + mispredictions + "\n");
-                    myWriter.write("misprediction rate:\t" + String.format("%.2f", (double) (100.0 * mispredictions) / accesses) + "%\n");
-                    myWriter.write("FINAL COUNTER CONTENT:\t");
+                    myWriter.write("misprediction rate:\t\t" + String.format("%.2f", (double) (100.0 * mispredictions) / accesses) + "%\n");
+                    myWriter.write("FINAL COUNTER CONTENT:\t\t");
                     myWriter.write(SP.print());
                     myWriter.close();
                     break;
@@ -58,6 +56,7 @@ class sim {
                     // initialize values
                     int M2 = Integer.parseInt(args[1]);
                     tracefileString = args[2];
+
                     fileReader = new BufferedReader(new FileReader(tracefileString));
                     accesses = 0;
                     mispredictions = 0;
@@ -79,12 +78,13 @@ class sim {
                             mispredictions++;
                     }
 
+                    // print results
                     myWriter.write("COMMAND\n");
                     myWriter.write("./sim bimodal " + M2 + " " + tracefileString + "\n");
                     myWriter.write("OUTPUT\n");
-                    myWriter.write("number of predictions:\t" + accesses + "\n");
+                    myWriter.write("number of predictions:\t\t" + accesses + "\n");
                     myWriter.write("number of mispredictions:\t" + mispredictions + "\n");
-                    myWriter.write("misprediction rate:\t" + String.format("%.2f", (double) (100.0 * mispredictions) / accesses) + "%\n");
+                    myWriter.write("misprediction rate:\t\t" + String.format("%.2f", (double) (100.0 * mispredictions) / accesses) + "%\n");
                     myWriter.write("FINAL BIMODAL CONTENTS");
                     myWriter.write(BP.print());
                     myWriter.close();
@@ -95,6 +95,7 @@ class sim {
                     int M1 = Integer.parseInt(args[1]);
                     int N = Integer.parseInt(args[2]);
                     tracefileString = args[3];
+
                     fileReader = new BufferedReader(new FileReader(tracefileString));
                     accesses = 0;
                     mispredictions = 0;
@@ -117,12 +118,13 @@ class sim {
                             mispredictions++;
                     }
 
+                    // print results
                     myWriter.write("COMMAND\n");
                     myWriter.write("./sim gshare " + M1 + " " + N + " " + tracefileString + "\n");
                     myWriter.write("OUTPUT\n");
-                    myWriter.write("number of predictions:\t" + accesses + "\n");
+                    myWriter.write("number of predictions:\t\t" + accesses + "\n");
                     myWriter.write("number of mispredictions:\t" + mispredictions + "\n");
-                    myWriter.write("misprediction rate:\t" + String.format("%.2f", (double) (100.0 * mispredictions) / accesses) + "%\n");
+                    myWriter.write("misprediction rate:\t\t" + String.format("%.2f", (double) (100.0 * mispredictions) / accesses) + "%\n");
                     myWriter.write("FINAL GSHARE CONTENTS");
                     myWriter.write(GP.print());
                     myWriter.close();
@@ -139,7 +141,7 @@ class sim {
                     fileReader = new BufferedReader(new FileReader(tracefileString));
                     accesses = 0;
                     mispredictions = 0;
-                    hybridPredictor HP = new hybridPredictor(1 << K, hM1, hN, hM2);
+                    hybridPredictor HP = new hybridPredictor(1 << K, 1 << hM1, hN, 1 << hM2);
                     // initialize accesses and mispredictions
                     while ((lineString = fileReader.readLine()) != null) {
                         // split line
@@ -156,12 +158,13 @@ class sim {
                             mispredictions++;
                     }
 
+                    // print results
                     myWriter.write("COMMAND\n");
                     myWriter.write("./sim hybrid " + K + " " + hM1 + " " + hN + " " + hM2 + " " + tracefileString + "\n");
                     myWriter.write("OUTPUT\n");
-                    myWriter.write("number of predictions:\t" + accesses + "\n");
+                    myWriter.write("number of predictions:\t\t" + accesses + "\n");
                     myWriter.write("number of mispredictions:\t" + mispredictions + "\n");
-                    myWriter.write("misprediction rate:\t" + String.format("%.2f", (double) (100.0 * mispredictions) / accesses) + "%\n");
+                    myWriter.write("misprediction rate:\t\t" + String.format("%.2f", (double) (100.0 * mispredictions) / accesses) + "%\n");
                     myWriter.write("FINAL CHOOSER CONTENTS");
                     myWriter.write(HP.print());
                     myWriter.close();
@@ -173,7 +176,7 @@ class sim {
     }
 }
 
-class basicPredictor {
+class basicPredictor {// this is a class to represent the most basic bit counters
     public int max, min, current, flip;
 
     public basicPredictor() {
@@ -184,7 +187,7 @@ class basicPredictor {
         //x = 1 << Bits = 2^Bits
         max = x - 1;
         min = 0;
-        flip = x / 2;
+        flip = x / 2;// this is the midway point, i.e. 1 for 1 bit, 2 for 2 bit, 4 for 3 bit, etc.
         current = flip;
     }
 
@@ -205,7 +208,7 @@ class basicPredictor {
     }
 }
 
-class smithPredictor {
+class smithPredictor {// this is basically a wrapper for the basicPredictor
     basicPredictor BP;
 
     public smithPredictor(int x) {
@@ -225,7 +228,7 @@ class smithPredictor {
     }
 }
 
-class bimodalPredictor {
+class bimodalPredictor {// this is basically a wrapper since bimodal is just a more specific gshare predictor
     gsharePredictor GP;
 
     public bimodalPredictor(int m) {
@@ -256,27 +259,26 @@ class gsharePredictor {
         nBits = n;
         GBHR = 0;
         predictionTable = new basicPredictor[m];
-        //TODO parallelize this loop if takes more than 2 minutes
+
         for (int i = 0; i < predictionTable.length; i++) {
             predictionTable[i] = new basicPredictor();
         }
     }
 
     public boolean call(int PC) {
-        //1. PC >>> 2    : discard 2 bottom bits of PC
+        //1. PC >>> 2   : discard 2 bottom bits of PC
         //2. & mMask    : get m low-order bits (aka, bits m+1 through 2)
         //3. ^ GBHR     : XOR with global branch history; in bimodal there will be no change (XOR 0 does nothing)
         index = ((PC >>> 2) & mMask) ^ GBHR;
         return predictionTable[index].predict();
     }
 
-    public void updateBP(boolean actualResult) {
+    public void updateBP(boolean actualResult) {// update branch predictor
         // !!! ASSUMES compiler doesn't switch operating order, uses index calculated by prior call()
-        // IF there are errors, have call() pass back the index calculated along with the prediction and pass index into here
         predictionTable[index].update(actualResult);
     }
 
-    public void updateGBHR(boolean actualResult) {
+    public void updateGBHR(boolean actualResult) {// update global branch history register
         // if there is no global history register, return
         if (nBits == 0) {
             return;
@@ -293,10 +295,8 @@ class gsharePredictor {
     }
 
     public String print() {
-        //TODO convert to use StringBuffer if takes more than 2 minutes
         String retString = new String();
 
-        //TODO parallelize this loop if takes more than 2 minutes
         for (int i = 0; i < predictionTable.length; i++) {
             retString = retString.concat("\n" + i + "\t" + predictionTable[i].current);
         }
@@ -312,12 +312,14 @@ class hybridPredictor {
 
     public hybridPredictor(int k, int m1, int n, int m2) {
         // k = 1 << kBits = 2^kBits
-        gsPredictor = new gsharePredictor(1 << m1, n);
-        bmPredictor = new bimodalPredictor(1 << m2);
-        kMask = k - 1;
+        // m1 = 1 << m1Bits = 2^m1Bits
+        // m2 = 1 << m2Bits = 1^m2Bits
+        gsPredictor = new gsharePredictor(m1, n);
+        bmPredictor = new bimodalPredictor(m2);
+        kMask = k - 1;// in binary, this is a series of '1's k bits long
         chooser = new basicPredictor[k];
         int chooserBits = 1 << 2;
-        //TODO parallelize this loop if takes more than 2 minutes
+
         for (int i = 0; i < chooser.length; i++) {
             chooser[i] = new basicPredictor(chooserBits);
         }
@@ -325,10 +327,12 @@ class hybridPredictor {
 
     public boolean predict(int PC, boolean actualResult) {
         int chooserIndex = (PC >>> 2) & kMask;
+        // 1. Obtain two predictions
         boolean retG = gsPredictor.call(PC);
         boolean retB = bmPredictor.call(PC);
         boolean ret;
 
+        // 2. Use chooser table to pick a prediction
         if (chooser[chooserIndex].predict()) {
             ret = retG;
             gsPredictor.updateBP(actualResult);
@@ -337,24 +341,23 @@ class hybridPredictor {
             bmPredictor.updateBP(actualResult);
         }
 
+        // 3. Update global branch history register
         gsPredictor.updateGBHR(actualResult);
 
-        if (retB == retG) {
-            //break;
-        } else if (retG == actualResult) {
+        // 4. Update chooser table counter
+        if (retG == actualResult && retB != actualResult) {// gshare correct, bimodal incorrect
             chooser[chooserIndex].update(true);
-        } else {
+        } else if (retB == actualResult && retG != actualResult) {// bimodal correct, gshare incorrect
             chooser[chooserIndex].update(false);
         }
+        // if gshare and bimodal are both correct or both incorrect, do nothing
 
         return ret;
     }
 
     public String print() {
-        //TODO convert to use StringBuffer if takes more than 2 minutes
         String retString = new String();
 
-        //TODO parallelize this loop if takes more than 2 minutes
         for (int i = 0; i < chooser.length; i++) {
             retString = retString.concat("\n" + i + "\t" + chooser[i].current);
         }
